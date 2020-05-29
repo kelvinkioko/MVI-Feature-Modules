f5c9dcbec1854b46c5f2b34ed1518dfb792c3b9e
Kelvin Kioko <kelvinmkioko@gmail.com>
2020-05-30 01:49:51 +0300

    Addedd ktlint checker and formatter is available and active

--- app/src/main/java/com/transaction/mvi/MainActivity.kt
+++ app/src/main/java/com/transaction/mvi/MainActivity.kt
@@ -1,11 +1,9 @@
 package com.transaction.mvi
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import androidx.navigation.findNavController
-import androidx.navigation.ui.NavigationUI.setupWithNavController
+import androidx.appcompat.app.AppCompatActivity
 
-class MainActivity : AppCompatActivity(){
+class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
